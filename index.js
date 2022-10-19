5dfeeeca6c98fe47e0ddd4688b791187cfb2318a
Kacper Radzik <KacperRadzik@op.pl>
2022-10-19 15:25:21 +0200

    commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const logo1 = document.querySelector
+(".MC-logo");
+TweenMax.to(logo1, 2, {opacity: 0});
+
+const logo2 = document.querySelector
+(".main-text");
+TweenMax.to(logo2, 5, {opacity: 0});
+
+const logo3 = document.querySelector
+(".text-p");
+TweenMax.to(logo3, 5, {opacity: 0});
+
+const logo4 = document.querySelector
+("#left-logo");
+TweenMax.to(logo4, 8, {opacity: 0});
+
+const logo5 = document.querySelector
+("#right-logo");
+TweenMax.to(logo5, 8, {opacity: 0});
+
+const logo6 = document.querySelector
+(".left-logo-text");
+TweenMax.to(logo6, 11, {opacity: 0});
+
+const logo7 = document.querySelector
+(".right-logo-text");
+TweenMax.to(logo7, 11, {opacity: 0});
+
+const logo8 = document.querySelector
+("#burger-left");
+TweenMax.to(logo8, 13, {opacity: 0});
+
+const logo9 = document.querySelector
+("#burger-right");
+TweenMax.to(logo9, 13, {opacity: 0});
