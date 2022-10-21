c9cc1456fc9dd94537c354b68e0aae26457797c4
Kacper Radzik <KacperRadzik@op.pl>
2022-10-21 13:57:37 +0200

    changes

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const logo1 = document.querySelector
+/*const logo1 = document.querySelector
 (".MC-logo");
-TweenMax.to(logo1, 2, {opacity: 0});
+TweenMax.to(logo1, 15, {opacity: 0});
 
 const logo2 = document.querySelector
 (".main-text");
-TweenMax.to(logo2, 5, {opacity: 0});
+TweenMax.to(logo2, 12, {opacity: 0});
 
 const logo3 = document.querySelector
 (".text-p");
-TweenMax.to(logo3, 5, {opacity: 0});
+TweenMax.to(logo3, 9, {opacity: 0});
 
 const logo4 = document.querySelector
 ("#left-logo");
-TweenMax.to(logo4, 8, {opacity: 0});
+TweenMax.to(logo4, 6, {opacity: 0});
 
 const logo5 = document.querySelector
 ("#right-logo");
-TweenMax.to(logo5, 8, {opacity: 0});
+TweenMax.to(logo5, 6, {opacity: 0});
 
 const logo6 = document.querySelector
 (".left-logo-text");
-TweenMax.to(logo6, 11, {opacity: 0});
+TweenMax.to(logo6, 4, {opacity: 0});
 
 const logo7 = document.querySelector
 (".right-logo-text");
-TweenMax.to(logo7, 11, {opacity: 0});
+TweenMax.to(logo7, 4, {opacity: 0});
 
 const logo8 = document.querySelector
 ("#burger-left");
-TweenMax.to(logo8, 13, {opacity: 0});
+TweenMax.to(logo8, 2, {opacity: 0});
 
 const logo9 = document.querySelector
 ("#burger-right");
-TweenMax.to(logo9, 13, {opacity: 0});
+TweenMax.to(logo9, 2, {opacity: 0});*/
